1cddbacae4bf557bea704a683e282979dc66c1c9
Femi Ayodeji <femidotayodeji@gmail.com>
2022-03-13 01:41:51 +0100

    Running

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #! /usr/bin/env node
 const { program } = require('commander')
+const list = require('./commands/list')
 
 program
     .command('list')
     .description('List all the TODO tasks')
     .action(list)
+
+program.parse()
